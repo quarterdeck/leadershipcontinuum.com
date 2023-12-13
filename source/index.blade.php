1b8d89fe3f3205f444e53a8065a80e5701a68289
Kris Northfield <kris@quarterdeck.co.uk>
2023-12-13 17:22:53 +0000

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,8 @@
 @extends('_layouts.main')
 
 @section('header')
-    <div class="flex justify-around items-center py-10 px-20 z-10 relative pb-0">
+    <p class="text-center italic pt-2">A real world, pragmatic look at leadership in business.</p>
+    <div class="flex justify-around items-center mt-6 px-20 z-10 relative pb-0">
         <a href="https://quarterdeck.co.uk" target="_blank">
             <img src="/img/quarterdeck.svg" class="transition-transform duration-500 ease-in-out transform hover:rotate-180 w-10 h-10" alt="World Class Leadership Training Programmes" />
         </a>
