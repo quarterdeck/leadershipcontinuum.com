58a309e12dc015f88afdb7f3caef9c5ca98072dd
Kris Northfield <kris@quarterdeck.co.uk>
2023-12-13 14:47:12 +0000

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,15 +6,15 @@
             <img src="/img/quarterdeck.svg" class="transition-transform duration-500 ease-in-out transform hover:rotate-180 w-10 h-10" alt="World Class Leadership Training Programmes" />
         </a>
         <ul class="italic text-center divide-y divide-blue-300">
-            <li class="py-1">Articles</li>
-            <li class="py-1">Profiles</li>
-            <li class="py-1">Quotes</li>
+            <li class="py-1"><a href="/articles">Articles</a></li>
+            <li class="py-1"><a href="/leaders-digest">Leader's Digest</a></li>
+            <li class="py-1"><a href="/profiles">Profiles</a></li>
         </ul>
         <img src="/img/ouroboros.svg" class="max-w-sm" />
         <ul class="italic text-center divide-y divide-blue-300">
-            <li class="py-1">Skills</li>
-            <li class="py-1">Accreditation</li>
-            <li class="py-1">Training</li>
+            <li class="py-1"><a href="/skills">Skills</a></li>
+            <li class="py-1"><a href="/accreditation">Accreditation</a></li>
+            <li class="py-1"><a href="/quotes">Quotes</a></li>
         </ul>
         <img src="/img/{{ $page->season }}.svg" class="w-10 h-10" />
     </div>
