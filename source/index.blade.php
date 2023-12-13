79d8c1807a6edaec50f341ca32fc98e56b2ddf7f
Kris Northfield <kris@quarterdeck.co.uk>
2023-12-13 21:57:27 +0000

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,15 +6,15 @@
         <a href="https://quarterdeck.co.uk" target="_blank">
             <img src="/img/quarterdeck.svg" class="transition-transform duration-500 ease-in-out transform hover:rotate-180 w-10 h-10" alt="World Class Leadership Training Programmes" />
         </a>
-        <ul class="italic text-center divide-y divide-indigo-300">
+        <ul class="italic text-center divide-y divide-indigo-300 w-32">
             <li class="py-1 hover:text-indigo-500"><a href="/articles">Articles</a></li>
             <li class="py-1 hover:text-indigo-500"><a href="/leaders-digest">Leader's Digest</a></li>
             <li class="py-1 hover:text-indigo-500"><a href="/profiles">Profiles</a></li>
         </ul>
         <img src="/img/ouroboros-banner.svg" class="max-w-sm" />
-        <ul class="italic text-center divide-y divide-blue-300">
+        <ul class="italic text-center divide-y divide-blue-300 w-32">
             <li class="py-1 hover:text-indigo-500"><a href="/skills">Skills</a></li>
-            <li class="py-1 hover:text-indigo-500"><a href="/accreditation">Accreditation</a></li>
+            <li class="py-1 hover:text-indigo-500"><a href="/books">Books</a></li>
             <li class="py-1 hover:text-indigo-500"><a href="/quotes">Quotes</a></li>
         </ul>
         <img src="/img/{{ $page->season }}.svg" class="w-10 h-10" />
@@ -23,6 +23,7 @@
         <a href="/articles" class="px-3 relative hover:text-indigo-500 border-r-[1px] border-gray-900">Articles</a>
         <a href="/profiles" class="px-3 relative hover:text-indigo-500 border-r-[1px] border-gray-900">Profiles</a>
         <a href="/skills" class="px-3 relative hover:text-indigo-500 border-r-[1px] border-gray-900">Skills</a>
+        <a href="/books" class="px-3 relative hover:text-indigo-500 border-r-[1px] border-gray-900">Books</a>
         <a href="/quotes" class="px-3 relative hover:text-indigo-500">Quotes</a>
     </nav>
 @endsection
