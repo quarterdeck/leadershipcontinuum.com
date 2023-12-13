c5d972d8eaae4cbcdc0c273dad4071486b139010
Kris Northfield <kris@quarterdeck.co.uk>
2023-12-13 14:29:41 +0000

    wip

--- config.php
+++ config.php
@@ -3,7 +3,13 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'title' => 'Jigsaw',
+    'title' => 'The Leadership Continuum',
     'description' => 'Website description.',
     'collections' => [],
+    'season' => match(idate('n')){
+        3,4,5 => 'spring',
+        6,7,8 => 'summer',
+        9,10,11 => 'autumn',
+        12,1,2 => 'winter',
+    }
 ];
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,9 +1,21 @@
 @extends('_layouts.main')
 
 @section('header')
-    <div class="flex justify-between items-center py-10">
-        <img src="/img/quarterdeck.svg" class="transition-transform duration-500 ease-in-out transform hover:rotate-180 w-10 h-10" />
-        <img src="/img/ouroboros.svg" class="max-w-md" />
-        <img src="/img/quarterdeck.svg" class="transition-transform duration-500 ease-in-out transform hover:rotate-180 w-10 h-10" />
+    <div class="flex justify-around items-center py-10 px-20">
+        <a href="https://quarterdeck.co.uk" target="_blank">
+            <img src="/img/quarterdeck.svg" class="transition-transform duration-500 ease-in-out transform hover:rotate-180 w-10 h-10" alt="World Class Leadership Training Programmes" />
+        </a>
+        <ul class="italic text-center divide-y divide-blue-300">
+            <li class="py-1">Articles</li>
+            <li class="py-1">Profiles</li>
+            <li class="py-1">Quotes</li>
+        </ul>
+        <img src="/img/ouroboros.svg" class="max-w-sm" />
+        <ul class="italic text-center divide-y divide-blue-300">
+            <li class="py-1">Skills</li>
+            <li class="py-1">Accreditation</li>
+            <li class="py-1">Training</li>
+        </ul>
+        <img src="/img/{{ $page->season }}.svg" class="w-10 h-10" />
     </div>
 @endsection
